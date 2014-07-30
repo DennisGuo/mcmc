9fcaf93efc6602348b168d6d0fd0b4e9587f0225
DennisGuo <guohengxi.dennis@gmail.com>
2014-07-30 14:21:08 +0800

    修改了基本路由规则

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,8 +7,21 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    /**
+     *  首页
+     *   @return
+     */
+    public static  Result home(){
+        return ok(hello.render());
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static Result example() {
+        String msg = "Your new application is ready.";
+        return ok(example.render(msg));
     }
 
 }
---
+++ target/scala-2.11/src_managed/main/routes_routing.scala
@@ -0,0 +1,89 @@
+// @SOURCE:F:/www/mcmc/conf/routes
+// @HASH:0dc445c526e79341184673a7373712052538a146
+// @DATE:Wed Jul 30 13:43:54 CST 2014
+
+
+import play.core._
+import play.core.Router._
+import play.core.Router.HandlerInvokerFactory._
+import play.core.j._
+
+import play.api.mvc._
+import _root_.controllers.Assets.Asset
+import _root_.play.libs.F
+
+import Router.queryString
+
+object Routes extends Router.Routes {
+
+import ReverseRouteContext.empty
+
+private var _prefix = "/"
+
+def setPrefix(prefix: String) {
+  _prefix = prefix
+  List[(String,Routes)]().foreach {
+    case (p, router) => router.setPrefix(prefix + (if(prefix.endsWith("/")) "" else "/") + p)
+  }
+}
+
+def prefix = _prefix
+
+lazy val defaultPrefix = { if(Routes.prefix.endsWith("/")) "" else "/" }
+
+
+// @LINE:6
+private[this] lazy val controllers_Application_home0_route = Route("GET", PathPattern(List(StaticPart(Routes.prefix))))
+private[this] lazy val controllers_Application_home0_invoker = createInvoker(
+controllers.Application.home(),
+HandlerDef(this.getClass.getClassLoader, "", "controllers.Application", "home", Nil,"GET", """ Home page""", Routes.prefix + """"""))
+        
+
+// @LINE:7
+private[this] lazy val controllers_Application_example1_route = Route("GET", PathPattern(List(StaticPart(Routes.prefix),StaticPart(Routes.defaultPrefix),StaticPart("example"))))
+private[this] lazy val controllers_Application_example1_invoker = createInvoker(
+controllers.Application.example(),
+HandlerDef(this.getClass.getClassLoader, "", "controllers.Application", "example", Nil,"GET", """""", Routes.prefix + """example"""))
+        
+
+// @LINE:10
+private[this] lazy val controllers_Assets_at2_route = Route("GET", PathPattern(List(StaticPart(Routes.prefix),StaticPart(Routes.defaultPrefix),StaticPart("assets/"),DynamicPart("file", """.+""",false))))
+private[this] lazy val controllers_Assets_at2_invoker = createInvoker(
+controllers.Assets.at(fakeValue[String], fakeValue[String]),
+HandlerDef(this.getClass.getClassLoader, "", "controllers.Assets", "at", Seq(classOf[String], classOf[String]),"GET", """ Map static resources from the /public folder to the /assets URL path""", Routes.prefix + """assets/$file<.+>"""))
+        
+def documentation = List(("""GET""", prefix,"""controllers.Application.home()"""),("""GET""", prefix + (if(prefix.endsWith("/")) "" else "/") + """example""","""controllers.Application.example()"""),("""GET""", prefix + (if(prefix.endsWith("/")) "" else "/") + """assets/$file<.+>""","""controllers.Assets.at(path:String = "/public", file:String)""")).foldLeft(List.empty[(String,String,String)]) { (s,e) => e.asInstanceOf[Any] match {
+  case r @ (_,_,_) => s :+ r.asInstanceOf[(String,String,String)]
+  case l => s ++ l.asInstanceOf[List[(String,String,String)]]
+}}
+      
+
+def routes:PartialFunction[RequestHeader,Handler] = {
+
+// @LINE:6
+case controllers_Application_home0_route(params) => {
+   call { 
+        controllers_Application_home0_invoker.call(controllers.Application.home())
+   }
+}
+        
+
+// @LINE:7
+case controllers_Application_example1_route(params) => {
+   call { 
+        controllers_Application_example1_invoker.call(controllers.Application.example())
+   }
+}
+        
+
+// @LINE:10
+case controllers_Assets_at2_route(params) => {
+   call(Param[String]("path", Right("/public")), params.fromPath[String]("file", None)) { (path, file) =>
+        controllers_Assets_at2_invoker.call(controllers.Assets.at(path, file))
+   }
+}
+        
+}
+
+}
+     
